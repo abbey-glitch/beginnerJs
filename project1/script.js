87c82dcd17c28dec6f71763ed94bc48a42192ab1
abbey-glitch <abiodunonaolapi@gmail.com>
2023-09-25 14:28:17 +0100

    class activity

--- project1/script.js
+++ project1/script.js
@@ -4,6 +4,7 @@ note_template.setAttribute('onclick', 'remAttr()')
 note_template.setAttribute('action', "javascript:void()")
 note_template.classList.add('formCont')
 note_template.innerHTML = `<div id='noteCont'>
+                                <h3 id='info'></h3>
                                 <fieldset>
                                     <legend>Note App</legend>
                                      <fieldset>
@@ -12,12 +13,12 @@ note_template.innerHTML = `<div id='noteCont'>
                                 </fieldset>
                                 <fieldset>
                                     <legend>Content</legend>
-                                    <textarea name="" cols=30 rows=4 placeholder="enter your text content">
+                                    <textarea name="" id="content" cols=30 rows=4 placeholder="enter your text content">
                                     </textarea>
                                 </fieldset>
                                 <fieldset>
                                      <legend>Author</legend>
-                                     <input type="text" placeholder="name of author">
+                                     <input type="text" id="author" placeholder="name of author">
                                 </fieldset>
                                 <fieldset>
                                       <button type="submit" id="save">Save</button>
@@ -46,6 +47,30 @@ function remAttr(){
 }
 
 function saveNote(){
-   let note_title = document.getElementById('noteTitle').value
-   console.log(note_title)
+   let note_title = document.getElementById('noteTitle').value.trim()
+   let note_content = document.getElementById('content').value.trim()
+   let author = document.getElementById('author').value.trim()
+   let noteVault = localStorage.getItem('note')
+   let appStore = []
+   let msg = document.getElementById('info');
+   if(noteVault == null || noteVault == undefined){
+    //    there is no note in store
+       if(note_title.length == 0 || note_content.length == 0 || author.length == 0){
+           msg.textContent = 'you need to fill all the field'
+           msg.setAttribute('class', 'error')
+       }else{
+           appStore.push({note_title, note_content, author})
+           let feedback = JSON.stringify(appStore)
+        //    console.log(feedback)
+           localStorage.setItem('note', feedback)
+           msg.textContent = 'save success'
+           msg.setAttribute('class', 'success')
+       }
+   }else{
+    //  there is note get the note
+    old_note = JSON.parse(noteVault)
+    old_note.push({note_title, note_content, author})
+    localStorage.setItem('note', JSON.stringify(old_note))
+    console.log(old_note)
+   }
 }
